f7f88b07b0e57d2634f282c73b96d8d996db594f
Alkoun-web-design <hassam.sajid.dvlpr@gmail.com>
2026-02-10 05:05:16 +0500

    added some tech and corrected spellings

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -54,11 +54,16 @@ const otherProjects = [
             hostLink: 'https://project-5-ochre.vercel.app/'
         },
         {
-            name: 'AARC website re-designed',
+            name: 'AARC',
             description: <div className='mx-2 mt-2 mb-4'> 
-                <p className="m-2">The website for AARC was re-designed a year later using tailwind css, tailgrids templates, and also incorporating animations through state management using AlpineJS.</p> 
-                <p className="m-2">The design was much cleaner, with more use of white. There was a dilemma on whether i should use HTMX, Alpine.js, or React.js, and eventually settled on Alpine.js due to it being readily incorporated into the template. Much of the form functionality carried over from the old website to the redesigned website.</p>
+                <p className="m-2">A statically served website, built on HTML, CSS, Tailwind and JavaScript with Alpine.js for state management, and a Node.js backend with an Express.js server.</p>
+                <p className="m-2">With less JavaScript shipped to the client and more HTML web pages ready for crawling, the business will benefit from SEO and higher SERP.</p>
+                <p className="m-2">The challenge was to maintain interactivity and reactivity available in other libraries and frameworks while maintaining better crawlability of the website and its content.</p>
+                <p className="m-2">By maintaining the original stack and simply adding Alpine.js for reactivity, we were able to achieve the benefits of React without the drawbacks affecting SEO and build complexity, whilst using a cleaner UI and better UX for visitor satisfaction.</p>
+                <p className="m-2">I gained a better understanding and appreciation of server side rendered websites, as there was less complexity, npm packages and build size involved.</p>
             </div>,
+            image: '/images/projects/aarc.webp',
+            imageAlt: 'AARC website',
             stack: [
                 {name:'AlpineJS', image:'/images/tech-stack/alpinejs-icon.svg'},
                 {name:'NodeJS', image:'/images/tech-stack/nodejs-icon.svg'},
@@ -215,9 +220,9 @@ const otherProjects = [
             name: 'Counselling with Iqra',
             description: <div className='mx-2 mt-2 mb-4'> 
                 <p className='m-2'>A business website for a psychologist/psychotherapist to inform clients and to allow them to get in touch with them, with blogging capabilities</p>
-                <p className='m-2'>The Psycholgist Ms.Iqra Naz benefitted from being able to reach new client and also establish an online presence.</p>
+                <p className='m-2'>The Psychologist Ms.Iqra Naz benefitted from being able to reach new client and also establish an online presence.</p>
                 <p className='m-2'>I had created an earlier version with HTML, CSS and JavaScript and publisehd that as a wordpress theme to allow Ms. Iqra Naz to blog using wordpress.
-                 However the costs of hosting the website and paying for the domain name, added to her costs. The previous desing of the website was also lackluster.</p>
+                 However the charges of hosting the website and paying for the domain name, added to her costs. The previous design of the website was also lackluster.</p>
                 <p className='m-2'>I had completed a design course and re-designed the website but also deployed it to netlify to cut hosting costs. I utilized Nunjucks for templating to speed up development. The blogging was added through markdown files to the a folder that was automatically trigger a new static build for the website and include the latest blog.</p>                    
                 <p className='m-2'>I learnt to use 11ty to develop a Statically generated website (SSG) and also implementing markdown files for blogging on the website.</p>
             </div>,
@@ -239,7 +244,7 @@ const otherProjects = [
                 <p className="m-2">A business website for a subsidiary of Canam Steel Pvt Ltd. It was developed using the Divi Builder on wordpress.</p>
                 <p className="m-2">The business wanted the website to provide legitimacy and online presence for prospective partners, businesses and employees.</p>  
                 <p className="m-2">The challenge was to build a website within a tight deadline with a clean UI using wordpress.</p> 
-                <p className="m-2">After completing a UI and UX course on Udemy, I foucs entirely on the design and aspects of the website and opted to use a builder to ensure my focus would nto deviate towards programming and coding. For this instance, I opted to use the Divi Builder with their theme and custom made components.</p> 
+                <p className="m-2">After completing a UI and UX course on Udemy, I focus entirely on the design and aspects of the website and opted to use a builder to ensure my focus would not deviate towards programming and coding. For this instance, I opted to use the Divi Builder with their theme and custom made components.</p> 
                 <p className="m-2">I learnt to implement basic deisgn principles on web elements. I also gained a new appreciation of UI builders.</p>
             </div>,
             image: '/images/projects/ssi.webp',
@@ -292,7 +297,7 @@ const otherProjects = [
         {
             name: 'AWDev website',
             description: <div className='mx-2 mt-2 mb-4'> 
-                <p className="m-2">I rebuilt and redesigned my portfolio website to showcase the tech stack used, my experience, my projects with a little bit abont me.</p> 
+                <p className="m-2">I rebuilt and redesigned my portfolio website to showcase the tech stack used, my experience, my projects with a little bit about me.</p> 
                 <p className="m-2">As a freelancing web developer, A portfolio website is essential to not only communicate past experience and accomplishments, but also future ambitions and interests,</p> 
                 <p className="m-2">I needed a portfolio to not only communicate my expertise and value in web development, but also my interest and ambitions in XR development, however 3D models are much larger assets</p> 
                 <p className="m-2">I used React with Tailwind to create a grid layout. I am intrigued by space and the universe, and I used React Three Fiber to use 3d models to create a scene in the background. Finally i used dynamic imports to defer loading of 3D and audio assets to achieve faster load times.</p> 
@@ -331,7 +336,7 @@ const otherProjects = [
             description: <div className='mx-2 mt-2 mb-4'> 
                 <p className="m-2">My first React Three Fiber website, it was originally designed to wish someone personally on their birthday.</p> 
                 <p className="m-2">3D scenes in websites arent as frequent an experience, So to wish someone with a 3D orchestrated scene would be a different experience.</p>
-                <p className="m-2">The challenges revolved around using 3D assets with the least amount of impact on load times with the intention of communicating the message and atomosphere of a birthday party.</p>
+                <p className="m-2">The challenges revolved around using 3D assets with the least amount of impact on load times with the intention of communicating the message and atmosphere of a birthday party.</p>
                 <p className="m-2">The scene was initially inside a cafe, but the scene required too many assets, so a camera angle that focused on minimal 3D assets to communicate a birthday gathering was opted for.</p>
                 <p className="m-2">I learnt to orchestrate minimal 3D scenes to communicate the message, with compressed and optimized textures and 3D models to reduce loading times and resource consumption as much as possible.</p>
             </div>,
@@ -385,9 +390,9 @@ const otherProjects = [
         {
             name: 'Binance Cryptocurrency Table',
             description: <div className='mx-2 mt-2 mb-4'> 
-                <p className="m-2">A personal project that displays the value of spot cryptocurrencies on Binance, built using SolidJS through the Astro framework and Node.js on the backend that communicates with binance and the client through websockets.</p> 
+                <p className="m-2">A personal project that displays the value of spot cryptocurrencies on binance, built using solid.js through the astro.js framework and Node.js on the backend that communicates with binance and the client through websockets.</p> 
                 <p className="m-2">The project was built initially to aid in trading on cryptocurrencies on binance's platform.</p>
-                <p className="m-2">The challenge was to manipualte and base caluclations on Binance's own data every second on all spot cryptocurrencies, and then send the data back to the client for presentation.</p>
+                <p className="m-2">The challenge was to manipulate and base calculations on binance's own data every second on all spot cryptocurrencies, and then send the data back to the client for presentation.</p>
                 <p className="m-2">Signals improved performance by removing the shadow DOM update step, allowing quicker and effortless updates as compared to React. Websockets were used to ensure immediate updates and calculations on each incoming message.</p> 
                 <p className="m-2">The project allowed me to gain a higher understanding of working with data types, and mutating, storing and later displayiong the same data. However this led to an understanding that this project was ill-equipped to aid in trading.</p>
             </div>,
@@ -431,8 +436,8 @@ const otherProjects = [
             description: <div className='mx-2 mt-2 mb-4'> 
                 <p className="m-2">An e-commerce website selling one product with two variants, built on shopify's Headless e-commerce framework Hydrogen.</p>
                 <p className="m-2">The website offered the client to sell their product online to a niche market. Due to their familiarity of Shopify's dashboard, </p> 
-                <p className="m-2">The challenge was to provide the client with a quick e-commerce website and dashboard that htye could themselved manage.</p> 
-                <p className="m-2">I used Shopify's frontend framwework called hydrogen that is connected to the users shopify dahsboard through APIs. This allwoed for a quicker developemnt time and more familiarity for the client.</p> 
+                <p className="m-2">The challenge was to provide the client with a quick e-commerce website and dashboard that they could themselves manage.</p> 
+                <p className="m-2">I used Shopify's frontend framework called hydrogen that is connected to the users shopify dashboard through APIs. This allowed for a quicker development time and more familiarity for the client.</p> 
                 <p className="m-2">I learnt to develop and use Shopify's Hydrogen framework to quickly launch an e-commerce website.</p> 
             </div>,
             image: '/images/projects/eye-analyzer.webp',
@@ -440,7 +445,8 @@ const otherProjects = [
             stack: [
                 {name:'Hydrogen', image:'/images/tech-stack/hydrogen-icon.svg'},
                 {name:'Shopify', image:'/images/tech-stack/shopify-icon.svg'},
-                {name:'JavaScript', image:'/images/tech-stack/js-icon.svg'}
+                {name:'JavaScript', image:'/images/tech-stack/js-icon.svg'},
+                {name:'Tailwind', image:'/images/tech-stack/tailwind-icon.svg'},
             ],
             status: 'Complete',
             githubLink: '',
@@ -453,7 +459,7 @@ const otherProjects = [
                 <p className="m-2">The idea was to quickly establish the online presence of the business so that clients and partners can be assured that the entity exists aand is continuing operations.</p> 
                 <p className="m-2">The challenge was to establish a placeholder website within a few days.</p> 
                 <p className="m-2">I used React and tailwind CSS to quickly build a website with a blurred background to show the countours of a navigation and the feeling of a work in progress.</p> 
-                <p className="m-2">I learnt how to build th ewebsite using React and Tailwind CSS and then statically generate files to be hosted on a traditional web hsoting platform.</p> 
+                <p className="m-2">I learnt how to build the website using React and Tailwind CSS and then statically generate files to be hosted on a traditional web hosting platform.</p> 
             </div>,
             image: '/images/projects/mma-placeholder.webp',
             imageAlt: 'MMA placeholder website',
@@ -469,10 +475,10 @@ const otherProjects = [
             name: 'Mind Muscles Academy website',
             description: <div className='mx-2 mt-2 mb-4'> 
                 <p className="m-2">A complete website, CMS and user platform for tutors, counsellors, and soon students for a schooling business built in React, TypeScript, Node.js, express.js and MySQL and SQLite.</p> 
-                <p className="m-2">The website would be a all in one platform all tutors, admins, counsellors, and soon students for interacting with the academy. </p>
-                <p className="m-2">The challenges were numerous, as I had to integrate a react frontend with a node.js and express.js backend to serve content from a database and also allow secure logging in into unique roles for allowing profile edits, curriculum access and CMS capabailities under a tight deadline. This was also my first Typescript with React project. I also had to keep queries to the backedn and the database under a certain limit.</p>
+                <p className="m-2">The website would be a all in one platform for all tutors, admins, counsellors, and soon students for interacting with the academy. </p>
+                <p className="m-2">The challenges were numerous, as I had to integrate a react frontend with a node.js and express.js backend to serve content from a database and also allow secure logging in into unique roles for allowing profile edits, curriculum access and CMS capabilities under a tight deadline. This was also my first Typescript with React project. I also had to keep queries to the backedn and the database under a certain limit.</p>
                 <p className="m-2">Upon the insistence of the client, I used an AI to generate a website, however even with coding assistants the technical demands could not be met, so I learnt react patterns and storing passwords and sessions as well.</p> 
-                <p className="m-2">I learnt typescript with react, integrating a backend which responded efficiently to fetch calls using React Query, restricted routes in React which could only be accessed after emails and passwords matched, and user sessions could be gneerated and stored.</p>
+                <p className="m-2">I learnt typescript with react, integrating a backend which responded efficiently to fetch calls using React Query, restricted routes in React which could only be accessed after emails and passwords matched, and user sessions could be generated and stored.</p>
             </div>,
             image: '/images/projects/mma.webp',
             imageAlt: 'MMA website',
@@ -589,7 +595,7 @@ const otherProjects = [
                 <p className="m-2">A template for e-commerce websites to allow for quick development of other e-commerce websites.</p>  
                 <p className="m-2">The challenge was to build an e-commerce website that allowed good SEO, good customer experience according to their expectations, and quick development on the frontend and backend.</p>
                 <p className="m-2">I utilized Next.js strengths in SEO, and its server and client components, API routes, coupled with interactivity and reactivity using React. I also used Baymard Insititute's guides on E-commerce UX.</p>
-                <p className="m-2">I learnt the intricacies of e-commerce UX, Next.js API routes, payments processors and shopping cart state managemnt.</p>
+                <p className="m-2">I learnt the intricacies of e-commerce UX, Next.js API routes, payments processors and shopping cart state management.</p>
             </div>,
             image: '/images/projects/ecom-next.webp',
             imageAlt: "A Next.js e-commerce website",
